edd1fcfeab8d5bf9587bbcecb0e06e4f744b68ea
est5 <ghostrekon5500@ya.ru>
2022-11-28 20:23:46 +0300

    Add: stack controller blueprint

--- FlashCardsLib/Controller/StackController.cs
+++ FlashCardsLib/Controller/StackController.cs
@@ -1,7 +1,27 @@
-namespace FlashCardsLib.Controller
-{
-    public class StackController
-    {
+using FlashCardsLib.Model;
+using Npgsql;
 
+namespace FlashCardsLib.Controller;
+
+public class StackController
+{
+
+    private NpgsqlConnection _dbContext;
+    public StackController(NpgsqlConnection dbContext)
+    {
+        _dbContext = dbContext;
     }
+
+    public void GetAllStacks() => throw new NotImplementedException();
+
+    public void GetStackById(int stackId) => throw new NotImplementedException();
+
+    public void GetFlashcardsInStackById(int stackId) => throw new NotImplementedException();
+
+    public void AddFlashcardToStackById(Flashcard flashcard, int stackId) => throw new NotImplementedException();
+
+    public void DeleteStackById(int stackId) => throw new NotImplementedException();
+
+    public void RemoveCardFromStackById(int stackId, int cardId) => throw new NotImplementedException();
+
 }
