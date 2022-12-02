1f9c74ff074b936d622369cfc8a84c6bbd3bac8d
est5 <ghostrekon5500@ya.ru>
2022-12-02 15:18:33 +0300

    Add: get one flashcard

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello world");
+﻿using FlashCardsLib.Controller;
+using FlashCardsLib.DB;
+
+
+var cntrl = new FlashcardController(new Postgres().GetDB());
+var card = cntrl.GetFlashcardById(1);
+System.Console.WriteLine($"front:{card.Front} back:{card.Back}");
--- FlashCardsLib/Controller/FlashcardController.cs
+++ FlashCardsLib/Controller/FlashcardController.cs
@@ -11,7 +11,33 @@ public class FlashcardController
         _dbContext = dbContext;
     }
 
-    public void GetFlashcardById(int id) => throw new NotImplementedException();
+    public Flashcard GetFlashcardById(int id)
+    {
+        var card = new Flashcard("", "");
+        using (_dbContext)
+        {
+            _dbContext.Open();
+
+            var command = _dbContext.CreateCommand();
+            command.CommandText = @"
+            SELECT front,back FROM flashcard
+            WHERE id = @id
+            ";
+
+            NpgsqlParameter param = new("@id", NpgsqlTypes.NpgsqlDbType.Integer);
+            param.Value = id;
+            command.Parameters.Add(param);
+
+            command.Prepare();
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                card.Front = (string)reader[0];
+                card.Back = (string)reader[1];
+            }
+        }
+        return card;
+    }
 
     public void GetAllFlashcards() => throw new NotImplementedException();
 
